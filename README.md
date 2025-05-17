cdc7958e76182bd7977f5852f153043b1c8bd643
Igor Leandro Dantas Medeiros <igtag.code@gmail.com>
2025-05-17 17:39:58 -0300

    dia 5 Git

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # clone-tabnews.com.br
 Projeto base do curso.dev para implementar o tabnews.com.br (clone)
+
+#Dia 5 Aula 1 Git
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+const ola = () =>{
+  return( 
+  <>
+  <h1>Hello World!</h1>
+  <p>Teste</p>
+  </>
+  );
+}
+
+export default ola;
